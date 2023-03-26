4b7012445294791b19b36d07ca7269a9b3f4d973
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-03-26 02:23:49 -0600

    Style - login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  clear: boolean = false;
+  sending = false;
+  fecha = new Date();
+  passwordTyping = false;
+  usuarioTyping = false;
+
+  constructor() {
+    this.form = new FormGroup({
+      usuario: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    // (<any>Object).values((<FormGroup>this.form).controls).forEach(campo => {
+    //   campo.markAsTouched();
+    // });
+
+    // if (this.form.valid) {
+    //   showLoader(true);
+    //   this.sending = true;
+    //   this._loginService.login(this.form.value.usuario, this.form.value.password)
+    //     .subscribe((d: boolean) => {
+    //         showLoader(false);
+    //         this._loginService.setUserRepartidorInLocalStorage(this.form.value.usuario);
+    //         this.sending = false;
+    //         this._router.navigate(['/app']);
+    //         if (d) {
+    //           this.Notificacion.duration = 500;
+    //           this.Notificacion.showNotification('Sesion iniciada', 'success');
+    //         }
+    //       },
+    //       e => {
+    //         this.captchaRef.reset();
+    //         this.sending = false;
+    //         catchError(e);
+    //       });
+    // }
+  }
+
+  submit() {
+    // const usuario = this.form.value.usuario;
+    // const checkRepartidor = this._loginService.checkUserRepartidorInLocalStorage(usuario);
+    // if (environment.modoOnline) {
+    //   return checkRepartidor ? this.login() : this.captchaRef.execute();
+    // } else {
+    //   this.login();
+    // }
+  }
+
+  resolved(data: any) {
+    if (data) {
+      this.login();
+    }
   }
 
 }
