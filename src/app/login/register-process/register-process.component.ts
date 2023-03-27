3edf907230adf41058edc7ca399695c20bbbd4c9
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-03-27 17:59:37 -0600

    Feat - register

--- src/app/login/register-process/register-process.component.ts
+++ src/app/login/register-process/register-process.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-process',
   templateUrl: './register-process.component.html',
-  styleUrls: ['./register-process.component.scss']
+  styleUrls: ['./register-process.component.scss'],
 })
 export class RegisterProcessComponent implements OnInit {
+  form: FormGroup;
+  clear: boolean = false;
+  clearConfirm: boolean = false;
+  sending = false;
+  fecha = new Date();
+  passwordTyping = false;
+  usuarioTyping = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.form = new FormGroup({
+      usuario: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
+  ngOnInit(): void {}
+
+  register() {}
 }
