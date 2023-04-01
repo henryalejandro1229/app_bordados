0fc44a573149abb6e67e02e1d2dbde22c0d44fe2
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-04-01 17:49:56 -0600

    Fix - redirect

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _auth: AuthService, private router: Router) {
+    if(!this._auth.isAuth() && !this._auth.isAdmin())
+      this.router.navigate(['/home']);
+  }
 
   ngOnInit(): void {
   }
