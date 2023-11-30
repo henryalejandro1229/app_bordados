300449e435dd6c799674c31601f9950de69e84ae
Henry Alejandro <henryalejandro1229@gmail.com>
2023-11-29 21:59:36 -0600

    Fix - protocolo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const host = '127.0.0.1';
 export const environment = {
   production: true,
-  url: `http://sastrerialospajaritos.proyectowebuni.com/api`,
+  url: `https://sastrerialospajaritos.proyectowebuni.com/api`,
   urlImg: `http://sastrerialospajaritos.proyectowebuni.com/api/products/imageProducts/`,
   urlEmail: `https://sastrerialospajaritos-86e11a91fb91.herokuapp.com/bordados_app_backend`,
 };
